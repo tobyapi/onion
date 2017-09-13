7a3439df5e8827bc06bf30989b80cf1eddf4c1c3
TobiasGSmollett <landofscala@gmail.com>
2017-09-13 22:13:40 +0900

    add shadow proxy

---
+++ src/onion/shadow_proxy.cr
@@ -0,0 +1,16 @@
+require "raze"
+require "http"
+
+
+module Onion
+  class ShadowProxy < Raze::Handler
+    def initialize(backends : Array(URI))
+      @backends = backends.map { |backend| HTTP::Client.new(backend)  }
+    end
+
+    def call(ctx, done)
+      @backends.each { |backend| backend.exec(ctx.request) }
+      done.call
+    end
+  end
+end
